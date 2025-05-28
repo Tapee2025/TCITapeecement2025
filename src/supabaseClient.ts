a76ffa076dc5542166efaf4a9c9526c6b3cc37d5
Tapee2025 <csakhiya007@gmail.com>
2025-05-28 18:08:07 +0530

    Updated supabaseClient.ts

--- src/supabaseClient.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://spvwilyeuydjnhunracm.supabase.co';
-const supabaseKey = 'YOUR_ANON_KEY'; // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdndpbHlldXlkam5odW5yYWNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MDg4MTcsImV4cCI6MjA2Mzk4NDgxN30.ue_O-UNeAPeHAkeVPE87qw5-cWF7xuuJXbijGskA18M
-export const supabase = createClient(supabaseUrl, supabaseKey);
