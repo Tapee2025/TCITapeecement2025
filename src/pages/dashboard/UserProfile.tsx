74b8d9b97b7688164bc70a1701173e5634766026
Chirag Sakhiya <csakhiya007@gmail.com>
2025-06-08 19:56:04 +0530

    Updated RedeemRewards.tsx

--- src/pages/dashboard/UserProfile.tsx
+++ src/pages/dashboard/UserProfile.tsx
@@ -293,7 +293,7 @@ export default function UserProfile() {
           >
             {saving ? (
               <>
-                <LoadingSpinner size="sm" className="mr-2" />
+                <LoadingSpinner size="sm\" className="mr-2" />
                 Saving...
               </>
             ) : (
