907db4b7bd78c14887fc358292a6399035aa60e2
Chirag Sakhiya <csakhiya007@gmail.com>
2025-06-09 10:19:25 +0530

    Updated UserProfile.tsx

--- src/pages/dashboard/UserProfile.tsx
+++ src/pages/dashboard/UserProfile.tsx
@@ -351,7 +351,7 @@ export default function UserProfile() {
           >
             {saving ? (
               <>
-                <LoadingSpinner size="sm" className="mr-2" />
+                <LoadingSpinner size="sm\" className="mr-2" />
                 Saving...
               </>
             ) : (
