9f8b6c0ef883175a73efbe4556788524720f5a0b
Chirag Sakhiya <csakhiya007@gmail.com>
2025-06-09 13:52:22 +0530

    Updated DealerProfile.tsx

--- src/pages/dealer/DealerProfile.tsx
+++ src/pages/dealer/DealerProfile.tsx
@@ -412,7 +412,7 @@ export default function DealerProfile() {
           >
             {saving ? (
               <>
-                <LoadingSpinner size="sm" className="mr-2" />
+                <LoadingSpinner size="sm\" className="mr-2" />
                 Saving...
               </>
             ) : (
