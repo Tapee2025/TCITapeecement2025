a878c035a03ea6d1923c1f2b263598875a2403ef
Chirag Sakhiya <csakhiya007@gmail.com>
2025-06-09 13:35:58 +0530

    Updated Register.tsx

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -131,8 +131,8 @@ export default function Register() {
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {authError && (
           <div className="p-4 rounded-xl bg-red-50 border border-red-200 text-red-700 text-sm flex items-center">
-            <svg className="w-5 h-5 mr-2 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+            <svg className="w-5 h-5 mr-2 flex-shrink-0\" fill="currentColor\" viewBox="0 0 20 20">
+              <path fillRule="evenodd\" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z\" clipRule="evenodd" />
             </svg>
             {authError}
           </div>
@@ -405,7 +405,7 @@ export default function Register() {
         >
           {loading ? (
             <>
-              <LoadingSpinner size="sm" className="mr-2" />
+              <LoadingSpinner size="sm\" className="mr-2" />
               Creating Account...
             </>
           ) : (
